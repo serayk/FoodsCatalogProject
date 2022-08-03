1b4184632476a3c7ca47bfa3d21d18e6ffc08b9a
serayk <seraykaraefe@gmail.com>
2022-08-03 15:24:08 +0300

    Initial Commit

---
+++ CatalogProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatalogProject
+//
+//  Created by MacOS on 3.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
